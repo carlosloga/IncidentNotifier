76fedc244bbd3638bb630155df54db9f2e698a38
CarlosLoGa <carlosloga@gmail.com>
2013-05-17 11:19:55 +0200

    2
    subir ficheros

---
+++ www/negocio/utilitats.js
@@ -0,0 +1,11 @@
+﻿
+function phoneGapRun() {
+    return(navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/));
+}
+
+function mensaje(msg) {
+    if(phoneGapRun())
+        navigator.notification.alert(msg, null, 'Atenció');
+    else
+        alert(msg);
+}
---
+++ www/negocio/webService.js
@@ -0,0 +1,32 @@
+﻿
+// ********************* METODOS PARA EL ACCESO A WebServices Y OBTENCIÓN DE DATOS ********************************
+
+function LlamaWebService(sTipoLlamada, sUrl,sContentType, bCrossDom, sDataType, bProcData, bCache, nTimeOut, funcion, pasaParam) {
+    global_AjaxRESULTADO = null;
+    global_AjaxERROR = '';
+
+    $.ajax({
+        type: sTipoLlamada,
+        url: sUrl,
+        //data: sParametros,
+        contentType: sContentType,
+        crossDomain: bCrossDom,
+        dataType: sDataType,
+        processData: bProcData,
+        cache: bCache,
+        timeout: nTimeOut,
+        success: function (xml) {
+            global_AjaxRESULTADO = xml;
+            if (funcion != null) {
+                funcion(global_AjaxRESULTADO, pasaParam);
+            }
+            else return global_AjaxRESULTADO;
+        },
+        error: function (e, f, g) {
+            global_AjaxERROR = 'ERROR en LlamaWebService \r\n' + e.message + ' ' + e.Description + ' ' + f + ' ' + g + ' en ' + ws + '  ' + sUrl + ' amb ' + sParametros;
+            if (funcion != null) funcion(global_AjaxERROR, pasaParam);
+        },
+        async: false
+    });
+    return global_AjaxRESULTADO;
+}
