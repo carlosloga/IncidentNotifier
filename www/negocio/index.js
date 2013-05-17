e45b54b4ce71f72daa30011b1e8270bdefe3a13b
CarlosLoGa <carlosloga@gmail.com>
2013-05-17 12:52:20 +0200

    8
    evento textArea

--- www/negocio/index.js
+++ www/negocio/index.js
@@ -20,6 +20,13 @@ function deviceReady() {
         pictureSource = navigator.camera.PictureSourceType;
         destinationType = navigator.camera.DestinationType;
     }
+
+    // en pageNuevaIncidencia -------------------------------------------------------------------
+    //Al cerrarse el aordeón del Comentari, que se ponga el texto en su cabecera ----------------
+    $('#collapsibleComentario').bind('collapse', function () {
+        $('#labelComentari').text( $('#textareaComentari').val() );
+    });
+
     $(document).on("mobileinit", function () {
         alert('mobileInit');
     });
@@ -38,12 +45,6 @@ function abrirPagina(sPag) {
         changeHash: true
     });
 }
-
-// pageNuevaIncidencia -------------------------------------------------------------------
-//Al cerrarse el aordeón del Comentari, que se ponga el texto en su cabecera
-$('#collapsibleComentario').bind('collapse', function () {
-    $('#labelComentari').text( $('#textareaComentari').val() );
-    });
 
 // -------- ABRIR CAMARA PARA HACER FOTO --------------------------------------------------------
 function hacerFoto() {
@@ -74,41 +75,41 @@ function iniciaMapa(bAbrir) {
     zoom: 14,
     mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-map = new google.maps.Map(document.getElementById('divMapa'), mapOptions);
+    map = new google.maps.Map(document.getElementById('divMapa'), mapOptions);
 
-// Try HTML5 geolocation
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-        sDireccion = cogerDireccion(pos);
+            sDireccion = cogerDireccion(pos);
 
-        var infowindow = new google.maps.InfoWindow({
-            map: map,
-            position: pos,
-            content: '<div><table><tr><td style="font-size:x-small; font-weight:bold;">reportar incidència en </td></tr><tr><td style="font-size:x-small; font-weight:normal;">' + sDireccion + '</td></tr></table></div>',
-            maxWidth: 300
+            var infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: pos,
+                content: '<div><table><tr><td style="font-size:x-small; font-weight:bold;">reportar incidència en </td></tr><tr><td style="font-size:x-small; font-weight:normal;">' + sDireccion + '</td></tr></table></div>',
+                maxWidth: 300
+            });
+
+    var marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        title: 'incidència aquí'
         });
 
-var marker = new google.maps.Marker({
-    position: pos,
-    map: map,
-    title: 'incidència aquí'
-    });
+    map.setCenter(pos);
 
-map.setCenter(pos);
+    $('#labelDireccion').text(sDireccion);
 
-$('#labelDireccion').text(sDireccion);
+    $('#divMapa').gmap('refresh');
 
-$('#divMapa').gmap('refresh');
+    if (!bAbrir) $('#collapsibleLocalizacion').trigger('collapse');
 
-if (!bAbrir) $('#collapsibleLocalizacion').trigger('collapse');
-
-}, function () { handleNoGeolocation(true); });
-} else {
-    // Browser doesn't support Geolocation
-    handleNoGeolocation(false);
-    }
+    }, function () { handleNoGeolocation(true); });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+        }
 }
 
 function handleNoGeolocation(errorFlag) {
@@ -118,13 +119,13 @@ function handleNoGeolocation(errorFlag) {
     } else {
     content = 'Error: el seu navegador no soporta geolocalització';
     }
-var options = {
-    map: map,
-    position: new google.maps.LatLng(41.97430, 2.78241),
-    content: content
-    };
-var infowindow = new google.maps.InfoWindow(options);
-map.setCenter(options.position);
+    var options = {
+        map: map,
+        position: new google.maps.LatLng(41.97430, 2.78241),
+        content: content
+        };
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
 }
 
 function cogerDireccion(pos) {
@@ -132,16 +133,17 @@ function cogerDireccion(pos) {
     var llamaWS = "http://maps.googleapis.com/maps/api/geocode/xml?latlng=" + pos.toString().replace(" ", "").replace("(","").replace(")","") + "&sensor=true";
     //alert(llamaWS);
     datos = LlamaWebService('GET', llamaWS, 'application/x-www-form-urlencoded', true, 'xml', false, false, 10000, null, null);
-    if (global_AjaxERROR != '') alert(global_AjaxERROR);
+    if (global_AjaxERROR != '')
+        alert(global_AjaxERROR);
     else
     {
-    sDireccion = $(datos).find('formatted_address').text();
-    var n = 0;
-    $(datos).find('formatted_address').each(function () {
-    if (n == 0) sDireccion = $(this).text();
-    n++;
-    });
-}
+        sDireccion = $(datos).find('formatted_address').text();
+        var n = 0;
+        $(datos).find('formatted_address').each(function () {
+        if (n == 0) sDireccion = $(this).text();
+        n++;
+        });
+    }
 return sDireccion;
 }
 
